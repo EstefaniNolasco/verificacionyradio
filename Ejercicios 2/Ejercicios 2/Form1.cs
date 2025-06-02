0bb5865df2f5c7fb9ecff18385b3bc7874e49b7a
EstefaniNolasco <nolascoestefani344@gmail.com>
2025-06-01 23:44:06 -0600

    Add files via upload

---
+++ Ejercicios 2/Ejercicios 2/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Ejercicios_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+           
+        }
+
+       
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            {
+                if (listBox1.SelectedItem != null) ;
+            }
+            {
+                txtResultado.Text = listBox1.SelectedItem.ToString();
+            }
+        }
+
+       
+
+      
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           listBox1.Items.Add("TITANIC");
+            listBox1.Items.Add("RAPIDOS Y FURIOSOS");
+            listBox1.Items.Add("LOS VENGADORES");
+            listBox1.Items.Add("AMERICAN PIE");
+            listBox1.Items.Add("HUGO");
+
+        }
+    }
+}
